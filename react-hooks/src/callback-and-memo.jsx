9b7c51cb40b5b4355f5b3913fc73a03c406bcc94
Hari Prasath <khariprasath30@gmail.com>
2024-09-21 16:49:14 +0530

    callback memo , usememo

--- react-hooks/src/callback-and-memo.jsx
+++ react-hooks/src/callback-and-memo.jsx
@@ -1,10 +1,24 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react'
 
+//  usecallback - prevent the function recreation every time possible.
+// if the function is updating the state - if premitive repeated value stopes automatically, but refernece type then we need use useCallback from stop the equation.
+
+
+//memo from react prevent the child component from rendering when parent componnet is get updated,
+
+//use callback - memoize functions,
+//use memo - memoize value,
+//memo - prevent the child for re-rendering.
+
+
+
 // callback hooks in react:
 const App = () => {
   const [count, setCount] = useState(0);
   const [todos, setTodos] = useState([]);
-  const [result,setResult] = useState(0)
+  const [result,setResult] = useState(0);
+
+
   const increment = () => {
     setCount((c) => c + 1);
   };
@@ -26,8 +40,7 @@ const App = () => {
       // fetchData()
   },[])
 
-//  usecallback - prevent the function recreation every time possible.
-// if the function is updating the state - if premitive repeated value stopes automatically, but refernece type then we need use useCallback from stop the equation.
+
 
 
   return (
